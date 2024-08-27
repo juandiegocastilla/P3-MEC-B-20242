1602d62481bf34c7925640dedb0fcb2b8a5bd847
juandiegocastilla <est.juan.castilla@unimilitar.edu.co>
2024-08-27 14:35:46 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,18 @@
+ */
+public class Fraccion {
+    public int numerador;
+    public int denominador;
+    {
+ void setnumerador(int numerador){
+  this.numerador=numerador};
+ 
+ void setdenominador(int denominador){
+ this.denominador=denominador};
+  
+ 
+ int getnumerador(){
+  return numerador};
+  int getdenominador(){
+    return denominador};
+    }
+}
